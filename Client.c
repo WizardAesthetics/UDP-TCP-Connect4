fb7d066fe818fd6c1b54f6a7e3764a187a1bea60
Blake Johnson <bjohn136@emich.edu>
2021-06-09 20:25:16 -0400

    Data Structs

--- Client.c
+++ Client.c
@@ -1,3 +1,4 @@
+//You should use the following definition for a client - to - server message :
 typedef struct {
 
 	enum { login, who, lookup, logout } requestType;     /* same size as an unsigned int */
@@ -8,10 +9,7 @@ typedef struct {
 
 } client2ServerMessage;                           /* an unsigned int is 32 bits = 4 bytes */
 
-
-
 //You should use the following definition for a server - to - client message :
-
 typedef struct {
 
 	enum { ok, lookup, who } responseType;        /* same size as an unsigned int */
@@ -26,7 +24,21 @@ typedef struct {
 
 } serverMessage;
 
+//You should use the following definition for a client - to - client message :
+typedef struct {
+
+	enum { play, accept, decline, move } requestType;    /* same size as an unsigned int */
+
+	unsigned int playerID;                 /* initiating player identifier */
+
+	unsigned int move;                     /* destination column – 1, 2, 3, 4, 5, 6, or 7 */
+
+} client2ClientMessage;                  /* an unsigned int is 32 bits = 4 bytes */
+
 int mian{
+
+
+
 
 
 	return 0;
